fc4633aa3bcc2ffacf846cda9886a2faaca41cb6
qkrwjdan <madogisa12@naver.com>
2019-12-11 14:42:44 +0900

    ok go

---
+++ 2/BlockComment.java
@@ -0,0 +1,12 @@
+/* 주석입니다요 */
+class BlockComment{
+    public static void main(String[] args)
+    {
+        System.out.println(7);
+        
+        System.out.println(3.15);
+        System.out.println("3 + 5 = " + 8);
+
+    }
+
+}
